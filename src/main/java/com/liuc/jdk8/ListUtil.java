b9b868b12952c71f13d6192e57444371b6fcff4b
liuc <15861805899@163.com>
2022-07-29 17:24:21 +0800

    提交

--- src/main/java/com/liuc/jdk8/ListUtil.java
+++ src/main/java/com/liuc/jdk8/ListUtil.java
@@ -10,14 +10,14 @@ import java.util.stream.Collectors;
 public class ListUtil {
 
     public static void main(String[] args) {
-        test2();
+//        test2();
     }
 
     public static List<User> testOne(){
         List<User> userList = new ArrayList<>();
-        userList.add(new User("张三",10,"男"));
-        userList.add(new User("王五",20,"女"));
-        userList.add(new User("刘",25,"男"));
+        userList.add(new User("张三",10,"男","178"));
+        userList.add(new User("王五",20,"女","168"));
+        userList.add(new User("刘",25,"男","173"));
         return userList;
     }
 
@@ -38,16 +38,34 @@ public class ListUtil {
 
     }
 
-
     public static void test2() {
         List<User> userList = testOne();
-        //提取List中的某一属性
+        /*
+         * 提取List中的某一属性
+         */
         List<String> nameList = userList.stream().map(User::getName).collect(Collectors.toList());
-        //log.info("提取List中的Name:{}", nameList);
 
-        //List过滤
+        /*
+         * List过滤
+         */
         List<User> list = userList.stream().filter(u -> u.getName().equals("王五")).collect(Collectors.toList());
-        log.info("提取List中的Name:{}", list);
+
+        //findAny()方法返回从任何元素流而使用
+        //findFirst()方法返回在第一个元素流
+        //orElse 查询不到返回指定值
+        User user3 = userList.stream().filter(u -> u.getSex().equals("男")).findAny().orElse(null);
+        User user2 = userList.stream().filter(u -> u.getSex().equals("男")).findFirst().orElse(new User());
+
+    }
+
+    public static void test3() {
+        List<User> userList = testOne();
+
+        /*
+         * 汇总操作
+         */
+        Integer reduce = userList.stream().map(User::getAge).reduce(0, Integer::sum);
+        log.info(":{}", reduce);
     }
 
 }
